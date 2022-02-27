6d83702f38fc4f23fcf1a47f7edaa5154032afff
Yoirishi <undertaker_zdv@mail.ru>
2022-02-27 23:29:19 +1000

    add manual check for result

--- printer.cpp
+++ printer.cpp
@@ -11,5 +11,16 @@ bool test()
     std::cout << testString<< std::endl;
     Printer printer;
 
-    printer.print(testString, false);
+    printer.print(testString, true);
+
+    char answer;
+    std::cout << "are strings above havent diffs?[y/n]" << std::endl;
+    do
+    {
+        std::cin >> answer;
+        if (answer != 'y' || answer != 'n')
+        {
+            std::cout << "incorrect answer" << std::endl;
+        }
+    }while (answer != 'y' || answer != 'n');
 }
